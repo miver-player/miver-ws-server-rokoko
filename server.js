d6699b6f62dc8e547254cb44484f6a86c35a009d
Tejaswi Gowda <tejaswi@asu.edu>
2023-09-16 13:58:23 -0700

    ac

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const port = 3000; //dparseInt(process.argv.slice(2));
 const wss1 = new WebSocket.Server({ noServer: true });
 const wss2 = new WebSocket.Server({ noServer: true });
 
-//for motive 3
+//for rokoko websocket
 wss1.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
+    console.log("received wss1: %s", message);
     var x = message.trim().split(" ").join(" ");
     //console.log(x);
     wss2.clients.forEach(function each(client) {
